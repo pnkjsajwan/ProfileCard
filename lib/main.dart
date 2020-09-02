68c428d11100c4cef6e6896dfbaf7f066d773d3e
pnkjsajwan <pankz.sajwan@outlook.com>
2020-09-02 16:44:18 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,40 +12,61 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 backgroundImage: AssetImage('images/ecardimg.jpg'),
+                radius: 100.0,
               ),
               Text(
                 'pnkjsajwan',
                 style: TextStyle(
-                    fontFamily: 'LongCang', fontWeight: FontWeight.bold),
+                    fontFamily: 'LongCang',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0),
               ),
               SizedBox(
                 height: 15.0,
               ),
               Text(
-                'Hybrid Apps Developer',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                'HYBRID APPS DEV',
+                style: TextStyle(fontSize: 12.0),
               ),
               SizedBox(
-                height: 15.0,
+                height: 20.0,
                 width: 130.0,
                 child: Divider(
                   height: 100.0,
+                  thickness: 2.0,
+                  color: Colors.deepOrange[900],
                 ),
               ),
               Card(
+                elevation: 5.0,
+                margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 5.0),
                 child: ListTile(
-                  leading: Icon(Icons.favorite_border),
-                  title: Text('github/pnkjsajwan'),
+                  leading: Icon(
+                    Icons.favorite_border,
+                    color: Colors.deepOrange[900],
+                  ),
+                  title: Text(
+                    'github/pnkjsajwan',
+                    style: TextStyle(fontSize: 14.0),
+                  ),
                 ),
               ),
               Card(
+                elevation: 5.0,
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
                 child: ListTile(
-                  leading: Icon(Icons.email),
-                  title: Text('pankz.sajwan@outlook.com'),
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.deepOrange[900],
+                  ),
+                  title: Text(
+                    'pankz.sajwan@outlook.com',
+                    style: TextStyle(fontSize: 14.0),
+                  ),
                 ),
               ),
             ],
