7bef9603915eab3b26e370b33847dc55b4b6d83f
pnkjsajwan <pankz.sajwan@outlook.com>
2020-09-02 16:23:35 +0530

    almost complete

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Row(
-          children: <Widget>[
-            CircleAvatar(
-              backgroundImage: AssetImage('images/ecardimg.jpg'),
-            )
-          ],
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/ecardimg.jpg'),
+              ),
+              Text(
+                'pnkjsajwan',
+                style: TextStyle(
+                    fontFamily: 'LongCang', fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                'Hybrid Apps Developer',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15.0,
+                width: 130.0,
+                child: Divider(
+                  height: 100.0,
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.favorite_border),
+                  title: Text('github/pnkjsajwan'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text('pankz.sajwan@outlook.com'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
